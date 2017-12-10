d480c2045b21539dc241773f2e1785867719424a
ghanime5550 <31673189+ghanime5550@users.noreply.github.com>
2017-12-10 16:53:59 -0500

    Add files via upload

---
+++ M5HW2_ RunningTotal_Ghanim.py
@@ -0,0 +1,14 @@
+# CTI-110
+# Eihab Ghanim
+# Total Runnig
+# 10/19/2017
+
+total=0
+newNumber = int (input("Enter a number: "))
+    
+while newNumber >= 0 :
+    total += newNumber
+    newNumber = int (input("Enter a number: "))
+print("The total is:",total)
+
+# End
